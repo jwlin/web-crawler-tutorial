56a074822ece0c1b8859056c847c58b1b2e20ee0
jwlin <cs.castman@gmail.com>
2017-03-04 21:59:27 -0300

    structure

--- ch1/connect.py
+++ ch1/connect.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 def main():
     resp = requests.get('http://blog.castman.net/web-crawler-tutorial/ch1/connect.html')
-    print(resp.text)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    print(soup.body.h1.text)
 
 
 if __name__ == '__main__':
