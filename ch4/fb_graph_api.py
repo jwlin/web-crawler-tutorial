ab9628c0cb3cd8c125836de0a5c62301fbb96c49
jwlin <cs.castman@gmail.com>
2017-03-30 03:36:37 +0800

    update

--- ch4/fb_graph_api.py
+++ ch4/fb_graph_api.py
@@ -1,38 +1,25 @@
 import requests
-import json
 
-ACCESS_TOKEN = ''
-page_id = 1707015819625206
-
-
-def get_me_info():
-    url = 'https://graph.facebook.com/v2.8/me?fields=id,name&access_token={}'.format(ACCESS_TOKEN)
-    res = requests.get(url)
-    print (res.text)
+ACCESS_TOKEN = 'EAACEdEose0cBAGIqZAGqWZAz84EdOJeMxob5LKaIwnHEPdZAApazqqIgeSe3FjDQra1hECIZBDGtkMV1ZCrsf3jZAOdKDLopV9i2XBXslvq3RsfNwZADG13cHqHkwB1LfjPCRZAbsHOsMphVVkBVwMt196b5mS7Fd40rcbGCSW5SQVDVNwrQUWaynTgZCUFQqH7ITPEseTx3J0jBCnodjkuB6YBt3b8sTYfYZD'
+page_id = 1707015819625206  # Pycone 松果城市粉絲專頁 id
 
 
 def get_me_friends():
     url = 'https://graph.facebook.com/v2.8/me?fields=id,name,friends&access_token={}'.format(ACCESS_TOKEN)
-    res = requests.get(url)
-    print (res.text)
+    data = requests.get(url).json()
+    print('My ID:', data['id'])
+    print('My name:', data['name'])
+    print('I have', data['friends']['summary']['total_count'], 'friends')
 
 
 def get_page_post():
     url = 'https://graph.facebook.com/v2.8/{0}/posts?access_token={1}'.format(page_id, ACCESS_TOKEN)
-    res = requests.get(url)
-    page_posts = json.loads(res.text)
-    for post in page_posts['data']:
-        try:
-            print ('post_id: {0}, post_created_time: {1}, post_message: {2}'.format(post['id'], post['created_time'], post['message']))
-        except Exception:
-            print ('post_id: {0}, post_created_time: {1}'.format(post['id'], post['created_time']))
-
-
-def main():
-    get_me_info()
-    #get_page_post()
-    get_me_friends()
+    data = requests.get(url).json()
+    print('粉絲頁有', len(data['data']), '篇貼文')
+    print('最新一篇時間:', data['data'][0]['created_time'])
+    print('內容:', data['data'][0]['message'])
 
 
 if __name__ == '__main__':
-    main()
+    get_me_friends()
+    get_page_post()
