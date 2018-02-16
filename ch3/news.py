5b580a7574a09de23579fc240824f85cf66079bd
jwlin <cs.castman@gmail.com>
2018-02-15 23:46:07 -0800

    update code

--- ch3/news.py
+++ ch3/news.py
@@ -6,7 +6,7 @@ def main():
     print('蘋果今日焦點')
     dom = requests.get('http://www.appledaily.com.tw/appledaily/hotdaily/headline').text
     soup = BeautifulSoup(dom, 'html5lib')
-    for ele in soup.find('ul', 'focus').find_all('li'):
+    for ele in soup.find('ul', 'all').find_all('li'):
         print(
             ele.find('div', 'aht_title_num').text,
             ele.find('div', 'aht_title').text,
