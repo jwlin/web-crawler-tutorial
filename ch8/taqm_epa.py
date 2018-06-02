addd71fd5a1e08772ebf453fbeb31571c910180e
Jun-Wei Lin <cs.castman@gmail.com>
2018-06-02 14:14:55 -0700

    URL to HTTPS and minor value changes in form data

--- ch8/taqm_epa.py
+++ ch8/taqm_epa.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    resp = requests.get('http://taqm.epa.gov.tw/taqm/tw/HourlyData.aspx')
+    URL = 'https://taqm.epa.gov.tw/taqm/tw/HourlyData.aspx'
+    resp = requests.get(URL)
     soup = BeautifulSoup(resp.text, 'html5lib')
     view_state = soup.find(id='__VIEWSTATE')['value']
     event_validation = soup.find(id='__EVENTVALIDATION')['value']
@@ -12,13 +13,13 @@ if __name__ == '__main__':
         '__VIEWSTATE': view_state,
         '__VIEWSTATEGENERATOR': viewstate_generator,
         '__EVENTVALIDATION': event_validation,
-        'ctl09$lbSite': '56',
-        'ctl09$lbParam': '4',
-        'ctl09$txtDateS': '2017/02/01',
-        'ctl09$txtDateE': '2017/03/31',
-        'ctl09$btnQuery': '查詢即時值'
+        'ctl04$lbSite': '56',
+        'ctl04$lbParam': '4',
+        'ctl04$txtDateS': '2018/05/01',
+        'ctl04$txtDateE': '2018/05/31',
+        'ctl04$btnQuery': '查詢即時值'
     }
-    resp = requests.post('http://taqm.epa.gov.tw/taqm/tw/HourlyData.aspx', data=form_data)
+    resp = requests.post(URL, data=form_data)
     soup = BeautifulSoup(resp.text, 'html5lib')
     for t in soup.find_all('table', 'TABLE_G'):
         print([s for s in t.stripped_strings])
