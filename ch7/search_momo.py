812752397c40b48923b05946680c81380961bb76
jwlin <junwel1@uci.edu>
2020-07-19 14:46:45 -0700

    update ch5/ezprice_csv.py

--- ch7/search_momo.py
+++ ch7/search_momo.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 def search_momo(query):
     query_enc = urllib.parse.quote(query)
-    url = "http://m.momoshop.com.tw/mosearch/" + query_enc + ".html"
+    url = "https://m.momoshop.com.tw/mosearch/" + query_enc + ".html"
     headers = {'User-Agent': 'mozilla/5.0 (Linux; Android 6.0.1; '
                              'Nexus 5x build/mtc19t applewebkit/537.36 (KHTML, like Gecko) '
                              'Chrome/51.0.2702.81 Mobile Safari/537.36'}
@@ -18,15 +18,13 @@ def search_momo(query):
     resp.encoding = 'utf-8'
     soup = BeautifulSoup(resp.text, 'html.parser')
     items = []
-    for elem in soup.find(id='itemizedStyle').ul.find_all('li'):
-        item_name = elem.find('p', 'prdName').text
-        item_price = elem.find("b", 'price').text.replace(',', '')
+    for elem in soup.find_all("li", "goodsItemLi"):
+        item_name = elem.find("h3", "prdName").text.strip()
+        item_price = elem.find("b", {"class": "price"}).text.strip()
         if not item_price:
             continue
-        item_price = int(item_price)
         item_url = 'http://m.momoshop.com.tw' + elem.find('a')['href']
-        item_img_url = elem.a.img['org']
-
+        item_img_url = elem.a.div.find_all("img")[-1]['src']
         item = {
             'name': item_name,
             'price': item_price,
@@ -38,9 +36,9 @@ def search_momo(query):
 
 
 if __name__ == '__main__':
-    query = 'iphone 7 128g plus'
+    query = 'iphone 11 128g'
     items = search_momo(query)
-    today = time.strftime('%m-%d')
+    today = time.strftime('%Y-%m-%d')
     print('%s 搜尋 %s 共 %d 筆資料' % (today, query, len(items)))
     for i in items:
         print(i)
