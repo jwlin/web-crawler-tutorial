90c132921ca747abe31bcf810543ec0331b2c4a3
jwlin <cs.castman@gmail.com>
2022-01-19 08:25:25 -0800

    ch8 updated

--- ch8/taqm_epa.py
+++ ch8/taqm_epa.py
@@ -2,7 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-if __name__ == '__main__':
+def airtw_epa():
+    URL = 'https://airtw.epa.gov.tw/CHT/Query/Month_Avg.aspx'
+    resp = requests.get(URL)
+    soup = BeautifulSoup(resp.text, 'html5lib')
+    view_state = soup.find(id='__VIEWSTATE')['value']
+    event_validation = soup.find(id='__EVENTVALIDATION')['value']
+    viewstate_generator = soup.find(id='__VIEWSTATEGENERATOR')['value']
+    form_data = {
+        '__VIEWSTATE': view_state,
+        '__VIEWSTATEGENERATOR': viewstate_generator,
+        '__EVENTVALIDATION': event_validation,
+        'ctl00$CPH_Content$ddl_Site': '5',
+        'ctl00$CPH_Content$ddlQYear': '2021',
+        'ctl00$CPH_Content$btnQuery': '查詢'
+    }
+    resp = requests.post(URL, data=form_data)
+    soup = BeautifulSoup(resp.text, 'html5lib')
+    for row in soup.find_all('tr'):
+        print([content for content in row.stripped_strings])
+
+
+def taqm_epa():
     URL = 'https://taqm.epa.gov.tw/taqm/tw/HourlyData.aspx'
     resp = requests.get(URL)
     soup = BeautifulSoup(resp.text, 'html5lib')
@@ -23,3 +44,8 @@ if __name__ == '__main__':
     soup = BeautifulSoup(resp.text, 'html5lib')
     for t in soup.find_all('table', 'TABLE_G'):
         print([s for s in t.stripped_strings])
+
+
+if __name__ == '__main__':
+    airtw_epa()
+    # taqm_epa() 網址已失效
